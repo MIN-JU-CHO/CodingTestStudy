1f1c270e748356b000e9172f1210ce10d76fba0d
MIN-JU-CHO <60171052+MIN-JU-CHO@users.noreply.github.com>
2023-11-03 19:13:12 +0900

    N과 M (1) 15649

---
+++ Project1/15649.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+
+int N, M;
+int arr[9];
+bool visited[10];
+
+void subsequence(int pos)
+{
+	if (pos == M + 1)
+	{
+		for (int i = 1; i < M + 1; ++i)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i = 1; i < N + 1; ++i)
+	{
+		if (visited[i] == false)
+		{
+			arr[pos] = i;
+			visited[i] = true;
+			subsequence(pos + 1);
+			visited[i] = false;
+		}
+	}
+}
+
+int main(void)
+{
+	cin >> N;
+	cin >> M;
+	subsequence(1);
+}
