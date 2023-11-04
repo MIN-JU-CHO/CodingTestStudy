a7a9a93f2188dd78e588ba19148e83857ba58c78
MIN-JU-CHO <60171052+MIN-JU-CHO@users.noreply.github.com>
2023-11-04 16:37:58 +0900

    1로 만들기 1463

---
+++ Project1/1463.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int N;
+int dp[1000001];
+using namespace std;
+int main(void)
+{
+	cin >> N;
+	dp[1] = 0;
+	for (int i = 2; i <= N; ++i)
+	{
+		dp[i] = dp[i - 1] + 1;
+		if (i % 3 == 0)
+		{
+			dp[i] = min(dp[i / 3] + 1, dp[i]);
+		}
+		if (i % 2 == 0)
+		{
+			dp[i] = min(dp[i / 2] + 1, dp[i]);
+		}
+	}
+	cout << dp[N];
+}
